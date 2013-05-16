6c28566e4e27df51ce21b47fd024c160f072c61c
Will Munn <willmunn@hotmail.com>
2013-05-16 17:34:16 +0100

    trying better fpm args

--- build-libgdiplus-package.bash
+++ build-libgdiplus-package.bash
@@ -23,22 +23,16 @@ wget "http://download.mono-project.com/sources/libgdiplus/libgdiplus-$LIB_GDI_PL
 tar -xvjf "libgdiplus-$LIB_GDI_PLUS_VERSION.tar.bz2"
 cd libgdiplus-$LIB_GDI_PLUS_VERSION 
 
+TARGET_DIR="$WORK_DIR/destdir"
+mkdir $TARGET_DIR
 ./configure
 make
 make install DESTDIR="$TARGET_DIR"
 cd $WORK_DIR
 
-fpm -d libpng-dev \
-	-d libX11-dev \
-	-d libcairo-dev \
-	-d libjpeg-dev \
-	-d libtiff-dev \
-	-d libgif-dev \
-	-s dir \
-	-t deb \
-	-n $LIB_GDI_PLUS_NAME \
-	-C $TARGET_DIR \
-	usr/bin usr/lib usr/share
+echo $TARGET_DIR
+echo $LIB_GDI_PLUS_NAME
+fpm -d "libpng-dev" -d "libX11-dev"	-d "libcairo-dev" -d "libjpeg-dev" -d "libtiff-dev" -d "libgif-dev" -s dir -t deb -n $LIB_GDI_PLUS_NAME -C $TARGET_DIR 	usr/bin usr/lib usr/share
 
 
 echo "Done. Your package should be ready in $WORK_DIR"
