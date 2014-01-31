ba505e82dc8335a13e61a6246bc75a57d4ca67f6
Sam Crang <sam.crang@7digital.com>
2014-01-31 10:21:54 +0000

    Spin up a Vagrant box to build mono in.

---
+++ Vagrantfile
@@ -0,0 +1,23 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+$provision = <<EOF
+apt-get update
+apt-get -y upgrade
+apt-get -y install autoconf libtool automake gettext
+apt-get -y install ruby ruby-dev rubygems
+apt-get -y install git
+apt-get -y install mono-gmcs
+gem update
+gem install fpm --no-ri --no-rdoc
+cd /vagrant
+sh build-mono-package.bash
+EOF
+
+VAGRANTFILE_API_VERSION = "2"
+
+Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
+  config.vm.box = "debian-wheezy-x64-7digital"
+  config.vm.box_url = "http://sam.nix.sys.7d/vagrant/debian-wheezy-x64-7digital.box"
+  config.vm.provision "shell", inline: $provision
+end
--- build-mono-package.bash
+++ build-mono-package.bash
@@ -1,52 +1,21 @@
 #!/bin/bash
 set -e
 
-die () {
-    echo >&2 "$@"
-    exit 1
-}
+mkdir -p out
+git clone -b app-config-fix git://github.com/7digital/mono.git --depth 1
+cd mono
 
-[ "$#" -eq 1 ] || die "Please specify the version of Mono you want to build as the argument. (Check the versions in the tarball list here: http://download.mono-project.com/sources/mono/)"
-
-which fpm > /dev/null || (echo "Please install fpm (from gem, not apt-get)" && exit 1)
-
-if mono --version > /dev/null 2>&1; then
-	echo "Mono is installed locally; please uninstall first" && exit 1
-fi
-
-WORK_DIR=/tmp/7digital-mono-work
-rm -rf $WORK_DIR
-mkdir $WORK_DIR
-cd $WORK_DIR
-
-MONO_VERSION=$1
-MONO_DIR="mono-$MONO_VERSION"
-
-SEVEND_VERSION="701"
-MONO7D_VERSION=$MONO_VERSION'.'$SEVEND_VERSION
-MONO7D_NAME="mono-7d"
-
-echo "Downloading $MONO_VERSION"
-wget http://download.mono-project.com/sources/mono/mono-$MONO_VERSION.tar.bz2
-
-tar -jxf mono-$MONO_VERSION.tar.bz2
-TARGET_DIR="$WORK_DIR/destdir"
-mkdir $TARGET_DIR
-
-cd "$WORK_DIR/$MONO_DIR"
-
-./configure --prefix=/usr
+./autogen.sh --prefix=/usr
 make
-make install DESTDIR="$TARGET_DIR"
-cd $WORK_DIR
+make install DESTDIR="`readlink -f ../out`"
 
 fpm -s dir \
     -t deb \
-    -n $MONO7D_NAME \
-    -v $MONO7D_VERSION \
-    -C $TARGET_DIR \
-    -d "libglib2.0-dev (>= 0)" \
-    usr/bin usr/lib usr/share usr/include usr/etc
-
-echo "Done. Your package should be ready in $WORK_DIR"
-
+    -C out \
+    --name "mono-7d" \
+    --version 3.2.6 \
+    --iteration "7$BUILD_NUMBER" \
+    --depends "libglib2.0-dev (>= 0)" \
+    --deb-user root \
+    --deb-group root \
+    --package mono-7d_VERSION-ITERATION_ARCH.deb usr/bin usr/lib usr/share usr/include usr/etc
